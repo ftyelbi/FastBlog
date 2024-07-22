3227a0d87a73a89e07f7face64fde62aa9c5bb5e
ftyelbi <ftyelbi@gmail.com>
2024-07-22 23:25:34 +0000

    updated

--- app/admin/addBlog/page.jsx
+++ app/admin/addBlog/page.jsx
@@ -12,7 +12,7 @@ const page = () => {
         title:"",
         description:"",
         category:"Startup",
-        author:"Alex Bennett",
+        author:"Abdul Fatahu",
         authorImg:"/author_img.png"
     })
 
@@ -40,7 +40,7 @@ const page = () => {
               title:"",
               description:"",
               category:"Startup",
-              author:"Alex Bennett",
+              author:"Abdul Fatahu",
               authorImg:"/author_img.png"
             });
         }
